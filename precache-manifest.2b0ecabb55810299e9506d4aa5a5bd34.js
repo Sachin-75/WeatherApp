4a2abd1c3250f0fa6dcc862c11d288d9b487e85c
Sachin-75 <sachin.kumavat93@gmail.com>
2024-06-15 14:30:47 +0530

    Updates

---
+++ precache-manifest.2b0ecabb55810299e9506d4aa5a5bd34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "800a55b303fbc0c0197e71a64296324d",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "c1e766ad0577fe7806b0",
+    "url": "/WeatherApp/static/css/main.1733834b.chunk.css"
+  },
+  {
+    "revision": "012849d609e4b21451c8",
+    "url": "/WeatherApp/static/js/2.2a036036.chunk.js"
+  },
+  {
+    "revision": "0eb3ca9d0061837f6c8f",
+    "url": "/WeatherApp/static/js/3.31c88699.chunk.js"
+  },
+  {
+    "revision": "c1e766ad0577fe7806b0",
+    "url": "/WeatherApp/static/js/main.4a608697.chunk.js"
+  },
+  {
+    "revision": "487d3c8ce8ef9e3702de",
+    "url": "/WeatherApp/static/js/runtime~main.4a67327f.js"
+  }
+]);
